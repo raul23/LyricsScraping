78feaecdb3dde346078e84f99e5691259364e203
Raul <rchfe23@gmail.com>
2019-08-28 22:42:42 -0400

    Update exceptions comments

--- scrapers/scraper_exceptions.py
+++ scrapers/scraper_exceptions.py
@@ -6,35 +6,37 @@ Extended module summary
 
 
 class InvalidURLCategoryError(Exception):
-    """The URL is not recognized as belonging to neither of the valid
-    categories"""
+    """Raised if the URL is not recognized as belonging to none of the valid
+    categories."""
 
 
 class InvalidURLDomainError(Exception):
-    """The URL's domain is invalid"""
+    """Raised if the URL's domain is invalid."""
 
 
 class MultipleAlbumError(Exception):
-    """Album's info extraction scheme broke: more than one album were found"""
+    """Raised if the album's info extraction scheme broke: more than one album
+    was found on the lyrics webpage."""
 
 
 class MultipleLyricsURLError(Exception):
-    """A lyrics URL was found more than once in the music db"""
+    """Raised if a lyrics URL was found more than once in the music db."""
 
 
 class NonUniqueAlbumYearError(Exception):
-    """Album's year extraction doesn't result in a UNIQUE number"""
+    """Raised if the album's year extraction doesn't result in a UNIQUE number."""
 
 
 class NonUniqueLyricsError(Exception):
-    """Lyrics extraction scheme broke: no lyrics found or more than one lyrics
-    were found"""
+    """Raised if the lyrics extraction scheme broke: no lyrics found or more
+    than one lyrics were found on the lyrics webpage."""
 
 
 class OverwriteSongError(Exception):
-    """A song was already found in the db and the db can't be updated"""
+    """Raised if a song was already found in the db and the db can't be updated
+    because updating the db is disabled by the user."""
 
 
 class WrongAlbumYearError(Exception):
-    """Album's year is in the wrong century: only songs published in the 20th
-    and 21th centuries are supported"""
+    """Raised if the album's year is in the wrong century: only songs
+    published in the 20th and 21th centuries are supported."""

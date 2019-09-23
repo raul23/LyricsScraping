d4dde526e920311674e2a7238ca559d53ff2bb10
Raul <rchfe23@gmail.com>
2019-09-23 18:44:39 -0400

    Add function in utils

---
+++ lyrics_scraping/utils.py
@@ -0,0 +1,58 @@
+"""Collection of utilities specifically for the lyrics scraping project.
+
+"""
+
+import os
+# Custom modules
+from lyrics_scraping import data
+
+
+def get_config_filepath(cfg_type, orig=False):
+    """Return the path to a configuration file.
+
+    The configuration file can either be:
+    - the main configuration file [1] : used for configuring a lyrics scraper
+    - the logging configuration file [2] : used to set up the logging for all
+                                           custom modules
+
+    By setting `orig` to True, you get instead the path to the original
+    configuration file with default values. Otherwise, you get the path to the
+    configuration file currently used by the program.
+
+    Parameters
+    ----------
+    cfg_type : str, {'log', 'main'}
+        The type of configuration file for which we want the path. 'log' refers
+        to the logging config file, and 'main' to the main config file used to
+        setup a lyrics scraper.
+    orig : bool, optional
+        Whether to get the path to the original configuration file (the default
+        value is False which implies that the path to the configuration file
+        currently used by the program will be returned).
+
+    Returns
+    -------
+    filepath : str
+        The path to the configuration file.
+
+    Raises
+    ------
+    AssertionError
+        Raised if te wrong type of configuration file is given to the function.
+        Only 'log' and 'main' are accepted for `cfg_type`.
+
+     References
+     ----------
+     .. [1] `TODO: add URL to main config file`_.
+     .. [2] `TODO: add URL to logging config file`_.
+
+    """
+    # TODO: add comments
+    assert cfg_type in ['log', 'main'], "Wrong type of config file"
+    if cfg_type == "log":
+        filename = 'orig_logging_cfg.yaml' if orig else 'logging_cfg.yaml'
+        filepath = os.path.join(data.__path__[0], filename)
+    else:
+        filename = 'orig_main_cfg.yaml' if orig else 'main_cfg.yaml'
+        filepath = os.path.join(data.__path__[0], filename)
+    return filepath

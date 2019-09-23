b65786efe159535a5963850a0413221180533622
Raul <rchfe23@gmail.com>
2019-09-23 18:30:40 -0400

    Implement reset_cfg script

--- bin/reset_cfg.py
+++ bin/reset_cfg.py
@@ -8,13 +8,39 @@ The configuration file can either be:
 
 Once the config file is reset, it will be updated with all the default values.
 
-TODO: add usage
+TODO: add script usage
 
 """
 
+import argparse
+# Custom modules
+from lyrics_scraping.utils import get_config_filepath
+from pyutils.genutils import copy_file
+
 
 def main():
-    pass
+    # Setup the parser
+    parser = argparse.ArgumentParser(
+        description="Reset a configuration file which can either be the "
+                    "logging or the main config file.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "-f", "--cfg_file",
+        required=True,
+        choices=["log", "main"],
+        help="What config file to reset: 'log' for the logging config file "
+             "and 'main' for the main config file.")
+    args = parser.parse_args()
+    # TODO: add comments
+    orig_cfg_filepath = get_config_filepath(cfg_type=args.cfg_file, orig=True)
+    user_cfg_filepath = get_config_filepath(cfg_type=args.cfg_file, orig=False)
+    try:
+        copy_file(source_filepath=orig_cfg_filepath,
+                  dest_filepath=user_cfg_filepath)
+    except OSError as e:
+        print(e)
+    else:
+        print("The {} configuration file is reset".format(args.cfg_file))
 
 
 if __name__ == '__main__':

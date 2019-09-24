3939b8c8ef82fd95628e255e1b8f35e7ebbf1bb1
Raul <rchfe23@gmail.com>
2019-09-23 21:16:24 -0400

    Specify Python versions in classifiers

--- setup.py
+++ setup.py
@@ -23,7 +23,9 @@ setup(name='LyricsScraping',
       classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries'
       ],

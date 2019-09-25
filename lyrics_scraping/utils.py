d97abc7d5daea4b2e0663afd11bebd1ab7b0c373
Raul <rchfe23@gmail.com>
2019-09-25 11:10:14 -0400

    - Change function name to get_data_filepath - Return also path to schema file

--- lyrics_scraping/utils.py
+++ lyrics_scraping/utils.py
@@ -7,51 +7,59 @@ import os
 from lyrics_scraping import data
 
 
-def get_config_filepath(cfg_type, default=False):
-    """Return the path to a user's configuration file.
+def get_data_filepath(file_type, default=False):
+    """Return the path to a data file used by the `lyrics_scraping` module.
 
-    The configuration file can either be:
-    - the main configuration file [1] : used for configuring a lyrics scraper
-    - the logging configuration file [2] : used to set up the logging for all
-                                           custom modules
+    The data file can either be the:
+    - SQL music schema file [1]: used for creating the SQLite database used for
+                                 storing the scraped data
+    - main configuration file [2] : used for configuring a lyrics scraper
+    - logging configuration file [3] : used to set up the logging for all
+                                       custom modules
 
     By setting `default` to True, you get instead the path to the configuration
     file with default values. Otherwise, you get the path to the configuration
     file currently used by the program, i.e. the one actually edited by the
-    user.
+    user. Note that `default` doesn't have any effect with the schema file.
 
     Parameters
     ----------
-    cfg_type : str, {'log', 'main'}
-        The type of configuration file for which we want the path. 'log' refers
-        to the logging config file, and 'main' to the main config file used to
-        setup a lyrics scraper.
-
+    file_type : str, {'log', 'main', 'schema'}
+        The type of data file for which we want the path. 'log' refers to the
+        logging config file, 'main' to the main config file used to setup a
+        lyrics scraper, and 'schema' to the SQL music schema file.
     default : bool, optional
-        Whether to get the path to the default configuration file (the default
+        Whether to get the path to the default configuration file. Note that
+        `default` doesn't have any effect with the schema file.(the default
         value is False which implies that the path to the configuration file
         currently used by the program will be returned).
 
     Returns
     -------
     filepath : str
-        The path to the configuration file.
+        The path to the data file.
 
     Raises
     ------
     AssertionError
-        Raised if te wrong type of configuration file is given to the function.
-        Only 'log' and 'main' are accepted for `cfg_type`.
+        Raised if the wrong type of data file is given to the function. Only
+        {'log', 'main', 'schema'} are accepted for `file_type`.
 
      References
      ----------
-     .. [1] `TODO: add URL to main config file`_.
-     .. [2] `TODO: add URL to logging config file`_.
+     .. [1] `TODO: add URL to SQL schema file`_.
+     .. [2] `TODO: add URL to main config file`_.
+     .. [3] `TODO: add URL to logging config file`_.
 
     """
-    assert cfg_type in ['log', 'main'], "Wrong type of config file"
-    if cfg_type == "log":
+    if file_type == "log":
         filename = 'default_logging_cfg.yaml' if default else 'logging_cfg.yaml'
-    else:
+    elif file_type == 'main':
         filename = 'default_main_cfg.yaml' if default else 'main_cfg.yaml'
+    elif file_type == 'schema':
+        filename = 'music.sql'
+    else:
+
+        raise AssertionError("Wrong type of data file: '{}' (choose from "
+                             "'log', 'main', 'schema')".format(file_type))
     return os.path.join(data.__path__[0], filename)

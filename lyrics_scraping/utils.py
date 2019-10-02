62ecc52c7283d68eef35fae2f5085dda43638635
Raul <rchfe23@gmail.com>
2019-10-02 18:36:42 -0400

    - Change function signature - Update function docstring

--- lyrics_scraping/utils.py
+++ lyrics_scraping/utils.py
@@ -1,40 +1,38 @@
 """Collection of utilities specifically for the lyrics scraping project.
 
+.. _default logging configuration file: https://bit.ly/2oPJSr4
+.. _default main configuration file: https://bit.ly/2n764MV
+.. _user-defined logging configuration file: https://bit.ly/2niTDgY
+.. _user-defined main configuration file: https://bit.ly/2oyt0VJ
+.. _SQL schema file music.sql: https://bit.ly/2kIMYvn
 """
 
 import os
 # Custom modules
 from lyrics_scraping import data
+from pyutils.genutils import list_to_str
 
 
-def get_data_filepath(file_type, default=False):
+def get_data_filepath(file_type):
     """Return the path to a data file used by the `lyrics_scraping` module.
 
     The data file can either be the:
 
-    - `SQL music schema file <https://bit.ly/2kIMYvn>`_: used for creating the
-      SQLite database used for storing the scraped data
-    - `main configuration file <https://bit.ly/2n764MV>`_: used for configuring
-      a lyrics scraper
-    - `logging configuration file <https://bit.ly/2oPJSr4>`_: used to set up
-      the logging for all custom modules
-
-    By setting `default` to True, you get instead the path to the configuration
-    file with default values. Otherwise, you get the path to the configuration
-    file currently used by the program, i.e. the one actually edited by the
-    user. Note that `default` doesn't have any effect with the schema file.
+    - **default_log**: refers to the `default logging configuration file`_ used
+      to setup the logging for all custom modules.
+    - **default_main**: refers to the `default main configuration file`_ used to
+      setup a lyrics scraper.
+    - **log**: refers to the `user-defined logging configuration file`_ which is
+      used to setup the logging for all custom modules.
+    - **main**: refers to the `user-defined main configuration file`_ used to
+      setup a lyrics scraper.
+    - **schema**: refers to the `SQL schema file music.sql`_ used for creating the SQLite
+      database which stores the scraped data.
 
     Parameters
     ----------
-    file_type : str, {'log', 'main', 'schema'}
-        The type of data file for which we want the path. 'log' refers to the
-        logging config file, 'main' to the main config file used to setup a
-        lyrics scraper, and 'schema' to the SQL music schema file.
-    default : bool, optional
-        Whether to get the path to the default configuration file. Note that
-        `default` doesn't have any effect with the schema file.(the default
-        value is False which implies that the path to the configuration file
-        currently used by the program will be returned).
+    file_type : str, {'default_log', 'default_main', 'log', 'main', 'schema'}
+        The type of data file for which we want the path.
 
     Returns
     -------
@@ -45,16 +43,18 @@ def get_data_filepath(file_type, default=False):
     ------
     AssertionError
         Raised if the wrong type of data file is given to the function. Only
-        {'log', 'main', 'schema'} are accepted for `file_type`.
+        {'default_log', 'default_main', 'log', 'main', 'schema'} are accepted
+        for `file_type`.
 
     """
-    if file_type == "log":
-        filename = 'default_logging_cfg.yaml' if default else 'logging_cfg.yaml'
-    elif file_type == 'main':
-        filename = 'default_main_cfg.yaml' if default else 'main_cfg.yaml'
-    elif file_type == 'schema':
-        filename = 'music.sql'
+    valid_file_types = ['default_log', 'default_main', 'log', 'main', 'schema']
+    assert file_type in valid_file_types, \
+        "Wrong type of data file: '{}' (choose from {})".format(
+            file_type, list_to_str(valid_file_types))
+    if file_type.endswith('log'):
+        filename = '{}ging_cfg.yaml'.format(file_type)
+    elif file_type.endswith('main'):
+        filename = '{}_cfg.yaml'.format(file_type)
     else:
-        raise AssertionError("Wrong type of data file: '{}' (choose from "
-                             "'log', 'main', 'schema')".format(file_type))
+        filename = 'music.sql'
     return os.path.join(data.__path__[0], filename)

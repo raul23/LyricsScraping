4036187a25eae6d5572cf8154bb4b230e59f9f53
Raul <rchfe23@gmail.com>
2019-10-14 18:30:59 -0400

    Add exceptions in lyrics_scraping/

---
+++ lyrics_scraping/exceptions.py
@@ -0,0 +1,52 @@
+"""Module that defines exceptions related to web scraping of lyrics websites.
+
+These are all the exceptions that are raised when running code from the
+`scrapers`_ package which defines the classes responsible for scraping
+lyrics websites.
+
+.. _scrapers:
+   https://github.com/raul23/LyricsScraping/tree/master/lyrics_scraping/scrapers
+
+"""
+
+
+class CurrentSessionURLError(Exception):
+    """Raised if the URL was already processed during the current session."""
+
+
+class InvalidURLCategoryError(Exception):
+    """Raised if the URL is not recognized as belonging to none of the valid
+    categories."""
+
+
+class InvalidURLDomainError(Exception):
+    """Raised if the URL's domain is invalid."""
+
+
+class MultipleAlbumError(Exception):
+    """Raised if the album extraction scheme broke: more than one album was
+    found on the lyrics webpage."""
+
+
+class MultipleLyricsURLError(Exception):
+    """Raised if a lyrics URL was found more than once in the music db."""
+
+
+class NonUniqueAlbumYearError(Exception):
+    """Raised if the album's year extraction scheme broke: no year found or
+    more than one year were found on the lyrics webpage."""
+
+
+class NonUniqueLyricsError(Exception):
+    """Raised if the lyrics extraction scheme broke: no lyrics found or more
+    than one lyrics were found on the lyrics webpage."""
+
+
+class OverwriteSongError(Exception):
+    """Raised if a song was already found in the db and the db can't be updated
+    because it is disabled by the user."""
+
+
+class WrongAlbumYearError(Exception):
+    """Raised if the album's year extraction scheme broke: the album's year is
+    not a number with four digits."""

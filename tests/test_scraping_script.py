3205648fe19ab56534aeb03e31440253fca08ec6
Raul <rchfe23@gmail.com>
2019-10-15 12:39:55 -0400

    Add test_edit_config_case_1

--- tests/test_scraping_script.py
+++ tests/test_scraping_script.py
@@ -5,46 +5,63 @@ command-line options.
 
 """
 
+import sys
+import unittest
+
 from .utils import TestLyricsScraping
 from lyrics_scraping.scripts import scraping
-from pyutils.genutils import get_qualname
+from pyutils.genutils import get_module_filename, get_qualname
 
 
 class TestScrapingScript(TestLyricsScraping):
     # TODO
-    TEST_MODULE_NAME = get_qualname(scraping)
+    TEST_MODULE_QUALNAME = get_qualname(scraping)
     LOGGER_NAME = __name__
     SHOW_FIRST_CHARS_IN_LOG = 0
 
-    # @unittest.skip("test_edit_config()")
-    def test_edit_config(self):
-        """TODO
+    # @unittest.skip("test_edit_config_case_1()")
+    def test_edit_config_case_1(self):
+        """Test that edit_config() opens the default app for editing the log
+        config file
+
+        Case 1 tests that :meth:`~lyrics_scraping.scripts.scraper.edit_config`
+        opens the default app for editing the logging config file.
+
+        For example, the default app as determined by the OS can be TextEdit or
+        atom.
+
         """
         self.log_test_method_name()
         self.logger.info("Testing <color>edit_config()</color>...")
+        sys.argv = [get_module_filename(scraping), '-e', 'log']
+        retcode = scraping.main()
+        msg = "The default app couldn't be open. Return code is " \
+              "{}".format(retcode)
+        self.assertTrue(retcode == 0, msg)
+        self.logger.info("The default app could be opened")
 
-    # @unittest.skip("test_reset_config()")
+    @unittest.skip("test_reset_config()")
     def test_reset_config(self):
         """TODO
         """
         self.log_test_method_name()
         self.logger.info("Testing <color>reset_config()</color>...")
 
-    # @unittest.skip("test_start_scraper()")
+    @unittest.skip("test_start_scraper()")
     def test_start_scraper(self):
         """TODO
         """
         self.log_test_method_name()
         self.logger.info("Testing <color>start_scraper()</color>...")
 
-    # @unittest.skip("setup_arg_parser()")
+    @unittest.skip("setup_arg_parser()")
     def test_setup_arg_parser(self):
         """TODO
         """
         self.log_test_method_name()
         self.logger.info("Testing <color>setup_arg_parser()</color>...")
 
-    # @unittest.skip("test_main()")
+    @unittest.skip("test_main()")
     def test_main(self):
         """TODO
         """

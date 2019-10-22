418e748f13ee4feda175d61a0b6945a986fc1731
Raul <rchfe23@gmail.com>
2019-10-22 00:38:35 -0400

    Add tests for undo and setup_argparser

--- tests/test_scraping_script.py
+++ tests/test_scraping_script.py
@@ -10,7 +10,7 @@ import sys
 import time
 import unittest
 
-from .utils import TestLyricsScraping, backup_modify_restore, move_and_restore
+from .utils import TestLyricsScraping, modify_and_restore, move_and_restore
 from lyrics_scraping.scripts import scraping
 from lyrics_scraping.utils import load_cfg
 from pyutils.genutils import get_module_filename, get_qualname
@@ -110,7 +110,7 @@ class TestScrapingScript(TestLyricsScraping):
                                expected_retcode=1, seconds=0)
 
     @unittest.skip("test_reset_config_case_1()")
-    @backup_modify_restore(cfg_type="log")
+    @modify_and_restore(cfg_type="log")
     def test_reset_config_case_1(self):
         """Test that reset_config() resets the log config file.
 
@@ -123,7 +123,7 @@ class TestScrapingScript(TestLyricsScraping):
         self._test_reset_config(args=['-r', 'log'])
 
     @unittest.skip("test_reset_config_case_2()")
-    @backup_modify_restore(cfg_type="main")
+    @modify_and_restore(cfg_type="main")
     def test_reset_config_case_2(self):
         """Test that reset_config() resets the main config file.
 
@@ -156,7 +156,7 @@ class TestScrapingScript(TestLyricsScraping):
                                 extra_main_msg=extra_msg)
 
     @unittest.skip("test_reset_config_case_4()")
-    @backup_modify_restore(cfg_type="main")
+    @modify_and_restore(cfg_type="main")
     def test_reset_config_case_4(self):
         """Test reset_config() when the main config file is reset twice in a
         row.
@@ -167,10 +167,27 @@ class TestScrapingScript(TestLyricsScraping):
         returns 2 when the main config file is reset twice in a row.
 
         """
-        self._test_two_reset_config('main')
+        # TODO: explain
+        cfg_type = "main"
+        # First RESET
+        extra_msg = "Case where the <color>{}</color> config file is <color>" \
+                    "reset twice</color> in a row\n".format(cfg_type)
+        extra_msg += "<color>RESET #1</color>"
+        self._test_reset_config(args=['-r', cfg_type],
+                                expected_retcode=0,
+                                extra_main_msg=extra_msg,
+                                undo_reset=False)
+        # Second RESET
+        extra_msg = "<color>RESET #2</color>"
+        self._start_newline = False
+        self._test_reset_config(args=['-r', cfg_type],
+                                expected_retcode=2,
+                                extra_main_msg=extra_msg)
+        self._start_newline = True
 
-    # @unittest.skip("test_undo_config_case_1()")
-    # @backup_modify_restore(cfg_type="log")
+    @unittest.skip("test_undo_config_case_1()")
+    @modify_and_restore(cfg_type="log", reset_first=True,
+                        check_modified_cfg=True)
     def test_undo_config_case_1(self):
         """Test that undo_config() restores the log config file.
 
@@ -181,56 +198,103 @@ class TestScrapingScript(TestLyricsScraping):
         last reset.
 
         """
-        import os
-        import shutil
-        from lyrics_scraping.utils import dump_cfg, get_bak_cfg_filepath, get_data_filepath
-        cfg_type = "log"
-        import ipdb
-        ipdb.set_trace()
-        # Copy the backup config file if it is there
-        found_backup_cfg = False
-        if os.path.isfile(get_bak_cfg_filepath(cfg_type)):
-            found_backup_cfg = True
-            shutil.copy(get_bak_cfg_filepath(cfg_type),
-                        get_bak_cfg_filepath(cfg_type) + ".tmp")
-        # Modify the log config file
-        shutil.copy(get_data_filepath(cfg_type),
-                    get_data_filepath(cfg_type) + ".tmp")
-        cfg_dict = load_cfg(cfg_type)
-        cfg_dict['new_option'] = 22
-        dump_cfg(get_data_filepath(cfg_type), cfg_dict)
         # Undo the config file
         self._test_undo_config(args=['-u', 'log'])
-        # Undo the changes to the config file
-        shutil.move(get_data_filepath(cfg_type) + ".tmp",
-                    get_data_filepath(cfg_type))
-        self.logger.info("{} config file restored".format(cfg_type))
-        # Restore the backup config file
-        if found_backup_cfg:
-            shutil.move(get_bak_cfg_filepath(cfg_type) + ".tmp",
-                        get_bak_cfg_filepath(cfg_type))
-            self.logger.info("Backup {} config file "
-                             "restored".format(cfg_type))
-        elif os.path.isfile(get_bak_cfg_filepath(cfg_type)):
-            # Remove backup config file since it wasn't there in the
-            # beginning
-            os.unlink(get_bak_cfg_filepath(cfg_type))
-            self.logger.info("Backup {} config file "
-                             "removed".format(cfg_type))
 
-    @unittest.skip("test_setup_arg_parser_case_1()")
-    def test_setup_arg_parser_case_1(self):
+    @unittest.skip("test_undo_config_case_2()")
+    @modify_and_restore(cfg_type="main", reset_first=True,
+                        check_modified_cfg=True)
+    def test_undo_config_case_2(self):
+        """Test that undo_config() restores the main config file.
+
+        TODO: check description
+
+        Case 2 tests that :meth:`~lyrics_scraping.scripts.scraper.undo_config`
+        restores the user-defined main config file to what it was before the
+        last reset.
+
+        """
+        # Undo the config file
+        self._test_undo_config(args=['-u', 'main'])
+
+    @unittest.skip("test_undo_config_case_3()")
+    @move_and_restore("main")
+    def test_undo_config_case_3(self):
+        """Test undo_config() when the main config file is not found.
+
+        TODO: check description, difficult to simulate this case
+
+        Case 3 tests that :meth:`~lyrics_scraping.scripts.scraper.undo_config`
+        returns 1 when the main config file is not found.
+
+        """
+        # TODO: explain
+        cfg_type = "main"
+        extra_msg = "Case where the <color>{}</color> config file is " \
+                    "<color>not found</color>".format(cfg_type)
+        # Undo the config file which is not to be found
+        self._test_undo_config(args=['-u', cfg_type],
+                               expected_retcode=1,
+                               extra_main_msg=extra_msg)
+
+    @unittest.skip("test_undo_config_case_4()")
+    @modify_and_restore(cfg_type="main", reset_first=True)
+    def test_undo_config_case_4(self):
+        """Test undo_config() when the main config file is undo twice in a
+        row.
+
+        TODO: check description
+
+        Case 4 tests that :meth:`~lyrics_scraping.scripts.scraper.reset_config`
+        returns 2 when the main config file is undo (restored) twice in a row.
+
+        """
+        # TODO: explain
+        cfg_type = "main"
+        # First UNDO
+        extra_msg = "Case where the <color>{}</color> config file is <color>" \
+                    "undo twice</color> in a row\n".format(cfg_type)
+        extra_msg += "<color>UNDO #1</color>"
+        self._test_undo_config(args=['-u', cfg_type],
+                               expected_retcode=0,
+                               extra_main_msg=extra_msg)
+        # Second UNDO
+        extra_msg = "<color>UNDO #2</color>"
+        self._start_newline = False
+        self._test_undo_config(args=['-u', cfg_type],
+                               expected_retcode=2,
+                               extra_main_msg=extra_msg)
+        self._start_newline = True
+
+    @unittest.skip("test_setup_argparser_case_1()")
+    def test_setup_argparser_case_1(self):
         """TODO
         """
-        self.log_test_method_name()
-        self.logger.info("Testing <color>setup_arg_parser()</color>...")
+        self._test_setup_argparser(['-e', 'log'], "edit")
 
-    @unittest.skip("test_setup_arg_parser_case_2()")
-    def test_setup_arg_parser_case_2(self):
+    @unittest.skip("test_setup_argparser_case_2()")
+    def test_setup_argparser_case_2(self):
         """TODO
         """
-        self.log_test_method_name()
-        self.logger.info("Testing <color>setup_arg_parser()</color>...")
+        self._test_setup_argparser(['-r', 'main'], "reset")
+
+    @unittest.skip("test_setup_argparser_case_3()")
+    def test_setup_argparser_case_3(self):
+        """TODO
+        """
+        self._test_setup_argparser(['-s'], "start_scraper")
+
+    @unittest.skip("test_setup_argparser_case_4()")
+    def test_setup_argparser_case_4(self):
+        """TODO
+        """
+        self._test_setup_argparser(['-u', 'log'], "undo")
+
+    @unittest.skip("test_setup_argparser_case_5()")
+    def test_setup_argparser_case_5(self):
+        """TODO
+        """
+        self._test_setup_argparser(['-x'], "invalid_argument")
 
     @unittest.skip("test_start_scraper()")
     def test_start_scraper(self):
@@ -239,7 +303,32 @@ class TestScrapingScript(TestLyricsScraping):
         self.log_test_method_name()
         self.logger.info("Testing <color>start_scraper()</color>...")
 
-    def _log_main_info_msg_for_cfg_method(self, args, extra_main_msg):
+    def _test_setup_argparser(self, args, expected_action, extra_msg=None):
+        self.log_test_method_name()
+        case, cfg_func = self.process_test_method_name()
+        info_msg = "Case <color>{}</color> of testing <color>{}()" \
+                   "</color>".format(case, cfg_func)
+        info_msg += "\n<color>Args:</color> {}".format(args)
+        self.logger.info(info_msg)
+        if extra_msg:
+            self.logger.info("\n{}".format(extra_msg))
+        sys.argv = [get_module_filename(scraping)]
+        sys.argv.extend(args)
+        try:
+            # Parse args
+            p_args = scraping.setup_argparser()
+            assert_msg = "The parsed arguments doesn't contain the expected " \
+                         "action '{}'".format(expected_action)
+            self.assertTrue(p_args.__contains__(expected_action), assert_msg)
+            self.logger.info("Parsed arguments <color>as expected</color>")
+        except SystemExit:
+            if expected_action == "invalid_argument":
+                self.logger.info("Invalid argument {} <color>as expected"
+                                 "</color>".format(args[0]))
+            else:
+                self.fail("Extremely odd case!")
+
+    def parse_args(self, args, extra_main_msg):
         """TODO
 
         Parameters
@@ -254,14 +343,15 @@ class TestScrapingScript(TestLyricsScraping):
         # TODO: explain
         self.log_test_method_name()
         case = self._testMethodName.split("case_")[-1]
-        valid_config_funcs = ["edit_config", "reset_config", "undo_config"]
-        config_func = self._testMethodName.split("test_")[1].split("_case")[0]
-        assert config_func in valid_config_funcs, \
+        valid_cfg_funcs = ["edit_config", "reset_config", "undo_config"]
+        cfg_func = self._testMethodName.split("test_")[1].split("_case")[0]
+        assert cfg_func in valid_cfg_funcs, \
             "Wrong config function detected: '{}' (valid functions are " \
-            "{})".format(config_func, ", ".join(valid_config_funcs))
+            "{})".format(cfg_func, ", ".join(valid_cfg_funcs))
         sys.argv = [get_module_filename(scraping)]
         sys.argv.extend(args)
-        p_args = scraping.setup_arg_parser()
+        # Parse args
+        p_args = scraping.setup_argparser()
         # Build main message for info logging
         if p_args.edit:
             cfg_type = p_args.edit
@@ -273,9 +363,9 @@ class TestScrapingScript(TestLyricsScraping):
         info_msg = "Case <color>{}</color> of testing <color>{}()</color> " \
                    "with the <color>{}</color> config file".format(
                     case,
-                    config_func,
+                    cfg_func,
                     cfg_type)
-        if config_func == "edit_config":
+        if cfg_func == "edit_config":
             app_type = "'{}'".format(p_args.app) if p_args.app else "default"
             info_msg += " and the <color>{}</color> app".format(app_type)
         else:
@@ -303,7 +393,7 @@ class TestScrapingScript(TestLyricsScraping):
 
         """
         # TODO: explain
-        results = self._log_main_info_msg_for_cfg_method(args, extra_main_msg)
+        results = self.parse_args(args, extra_main_msg)
         app_type = results.app_type
         # Call main() with the given arguments
         # NOTE: setup_arg_parser() is called in main()
@@ -311,17 +401,16 @@ class TestScrapingScript(TestLyricsScraping):
         if expected_retcode == 0:
             assert_msg = "The {} app couldn't be open. Return code is " \
                          "{}".format(app_type, retcode)
-            info_msg = "The {} app was called <color>as expected</color>"
             self.assertTrue(retcode == expected_retcode, assert_msg)
-            self.logger.info(info_msg.format(app_type))
+            self.logger.info("The {} app was called <color>as expected"
+                             "</color>".format(app_type))
             time.sleep(seconds)
         else:
             assert_msg = "Very highly unlikely case. Return code " \
                          "is".format(retcode)
-            info_msg = "The {} app couldn't be called <color>as expected" \
-                       "</color>".format(app_type)
             self.assertTrue(retcode == expected_retcode, assert_msg)
-            self.logger.info(info_msg)
+            self.logger.info("The {} app couldn't be called <color>as "
+                             "expected</color>".format(app_type))
 
     def _test_reset_config(self, args, expected_retcode=0, extra_main_msg=None,
                            undo_reset=True):
@@ -336,7 +425,7 @@ class TestScrapingScript(TestLyricsScraping):
 
         """
         # TODO: explain
-        results = self._log_main_info_msg_for_cfg_method(args, extra_main_msg)
+        results = self.parse_args(args, extra_main_msg)
         cfg_type = results.cfg_type
         # Call main() with the given arguments
         # NOTE: setup_arg_parser() is called within main()
@@ -360,41 +449,15 @@ class TestScrapingScript(TestLyricsScraping):
                 self.assertTrue(retcode == 0, assert_msg)
                 self.logger.info("The {} config file was successfully reverted "
                                  "<color>as expected</color>".format(cfg_type))
-        elif expected_retcode in [1, 2]:  # ERROR or Config file ALREADY reset
-            assert_msg = "Return code ({}) not as expected " \
-                         "({})".format(retcode, expected_retcode)
-            info_msg = "The {} config file couldn't be reset <color>as " \
-                       "expected</color>".format(cfg_type)
-            self.assertTrue(retcode == expected_retcode, assert_msg)
-            self.logger.info(info_msg)
+        elif expected_retcode == 1:  # ERROR
+            self.assert_retcode_when_fail(cfg_type, retcode, expected_retcode,
+                                           "reset")
+        elif expected_retcode == 2:  # Config file ALREADY reset
+            self.assert_retcode_when_fail(cfg_type, retcode, expected_retcode,
+                                           "reset")
         else:
             self.fail("Very odd! Return code ({}) not as expected "
                       "({})".format(retcode, expected_retcode))
-
-    def _test_two_reset_config(self, cfg_type):
-        """TODO
-
-        Parameters
-        ----------
-        cfg_type
-
-        """
-        # TODO: explain
-        # First RESET
-        extra_msg = "Case where the <color>{}</color> config file is <color>" \
-                    "reset twice</color> in a row\n".format(cfg_type)
-        extra_msg += "<color>RESET #1</color>"
-        self._test_reset_config(args=['-r', cfg_type],
-                                expected_retcode=0,
-                                extra_main_msg=extra_msg,
-                                undo_reset=False)
-        # Second RESET
-        extra_msg = "<color>RESET #2</color>"
-        self._start_newline = False
-        self._test_reset_config(args=['-r', cfg_type],
-                                expected_retcode=2,
-                                extra_main_msg=extra_msg)
-        self._start_newline = True
 
     def _test_undo_config(self, args, expected_retcode=0, extra_main_msg=None):
         """TODO
@@ -404,11 +467,47 @@ class TestScrapingScript(TestLyricsScraping):
         args
         expected_retcode
         extra_main_msg
-        undo_reset
 
         """
-        results = self._log_main_info_msg_for_cfg_method(args, extra_main_msg)
+        results = self.parse_args(args, extra_main_msg)
         cfg_type = results.cfg_type
         # Call main() with the given arguments
         # NOTE: setup_arg_parser() is called within main()
         retcode = scraping.main()
+        if expected_retcode == 0:  # Success: config file restored
+            assert_msg = "The {} config file couldn't be restored. Return " \
+                         "code is {}".format(cfg_type, retcode)
+            self.assertTrue(retcode == expected_retcode, assert_msg)
+            self.logger.info("The {} config file was successfully restored "
+                             "<color>as expected</color>".format(cfg_type))
+        elif expected_retcode == 1:  # ERROR
+            self.assert_retcode_when_fail(cfg_type, retcode, expected_retcode,
+                                           "restored")
+        elif expected_retcode == 2:  # Config file already restored
+            self.assert_retcode_when_fail(cfg_type, retcode, expected_retcode,
+                                           "restored")
+        else:
+            self.fail("Very odd! Return code ({}) not as expected "
+                      "({})".format(retcode, expected_retcode))
+
+    def assert_retcode_when_fail(self, cfg_type, retcode, expected_retcode, action):
+        """TODO
+
+        Parameters
+        ----------
+        cfg_type
+        retcode
+        expected_retcode
+        action
+
+        """
+        valid_expected_retcodes = [1, 2]
+        valid_as_str = ", ".join(map(str, valid_expected_retcodes))
+        assert expected_retcode in valid_expected_retcodes, \
+            "Wrong expected rectcode: '{}' (valid expected rectcodes are " \
+            "{})".format(expected_retcode, valid_as_str)
+        assert_msg = "Return code ({}) not as expected " \
+                     "({})".format(retcode, expected_retcode)
+        self.assertTrue(retcode == expected_retcode, assert_msg)
+        self.logger.info("The {} config file couldn't be {} <color>"
+                         "as expected</color>".format(cfg_type, action))
